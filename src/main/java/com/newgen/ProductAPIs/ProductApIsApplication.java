15a83b537f47f2a912fc82374b0ce6df5ea7a9a1
Prathamesh Gosavi <prathameshgosavi945@gmail.com>
2025-03-06 13:09:46 +0530

    add some file

---
+++ src/main/java/com/newgen/ProductAPIs/ProductApIsApplication.java
@@ -0,0 +1,20 @@
+package com.newgen.ProductAPIs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.w3c.dom.ls.LSOutput;
+
+import java.sql.SQLOutput;
+
+@SpringBootApplication
+public class ProductApIsApplication {
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(ProductApIsApplication.class, args);
+
+		System.out.println("hey romil bhai ");
+	}
+
+
+}
