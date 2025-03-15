f0df36ef101ac7a171dfc5dec3eb9a4734188818
Prathamesh Gosavi <prathameshgosavi945@gmail.com>
2025-03-15 23:44:16 +0530

    add testing repository

--- src/main/java/com/newgen/ProductAPIs/ProductApIsApplication.java
+++ src/main/java/com/newgen/ProductAPIs/ProductApIsApplication.java
@@ -1,6 +1,9 @@
 package com.newgen.ProductAPIs;
 
 import com.newgen.ProductAPIs.Service.ProductService;
+import com.newgen.ProductAPIs.model.Category;
+import com.newgen.ProductAPIs.model.Product;
+import com.newgen.ProductAPIs.repository.ProductRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -16,10 +19,11 @@ public class ProductApIsApplication {
         ApplicationContext applicationContext = SpringApplication.run(ProductApIsApplication.class, args);
 
 
-//		ProductService service= applicationContext.getBean(ProductService.class);
-//
-//		service.getAllProduct().forEach(System.out::println);
+        ProductRepository productRepository=applicationContext.getBean(ProductRepository.class);
 
+        productRepository.save(new Product("Kurti", Category.CLOTHES,540.90));
+
+        System.out.println("product add successfully");
 
     }
 
