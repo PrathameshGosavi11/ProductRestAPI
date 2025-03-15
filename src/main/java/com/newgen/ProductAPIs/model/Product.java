65b37e30c7b1e4e5ca1e579e5d91b840c64ca710
Prathamesh Gosavi <prathameshgosavi945@gmail.com>
2025-03-15 22:12:35 +0530

    add  configuraton and etc

--- src/main/java/com/newgen/ProductAPIs/model/Product.java
+++ src/main/java/com/newgen/ProductAPIs/model/Product.java
@@ -1,16 +1,28 @@
 package com.newgen.ProductAPIs.model;
 
+import jakarta.persistence.*;
 import lombok.*;
 
 
 @Setter
 @Getter
 @ToString
+@NoArgsConstructor
+@Entity
+@Table(name="Products")
 public class Product {
-    @Setter
-    private long id;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "Product_Name",updatable = false,nullable = false)
     private String name;
+
+    @Column(name="Product_Category")
     private Category category;
+
+    @Column(name="Product_Price")
     private double price;
 
     public Product(String name, Category category, double price) {
