748ccd078738a92856ead2a4eddd4996f590863b
Prathamesh Gosavi <prathameshgosavi945@gmail.com>
2025-03-15 22:22:10 +0530

    add @repository annotation

--- src/main/java/com/newgen/ProductAPIs/repository/ProductRepository.java
+++ src/main/java/com/newgen/ProductAPIs/repository/ProductRepository.java
@@ -2,7 +2,9 @@ package com.newgen.ProductAPIs.repository;
 
 import com.newgen.ProductAPIs.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductRepository extends  JpaRepository<Product ,Long>
 {
 
