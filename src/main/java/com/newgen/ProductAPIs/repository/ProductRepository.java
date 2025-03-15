e98cce111ec6fbb93ceb074a4a94f4273c756429
Prathamesh Gosavi <prathameshgosavi945@gmail.com>
2025-03-15 23:33:54 +0530

    add repository

--- src/main/java/com/newgen/ProductAPIs/repository/ProductRepository.java
+++ src/main/java/com/newgen/ProductAPIs/repository/ProductRepository.java
@@ -2,10 +2,14 @@ package com.newgen.ProductAPIs.repository;
 
 import com.newgen.ProductAPIs.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ProductRepository extends  JpaRepository<Product ,Long>
 {
-
+//    @Query(value="select p from Product p")
+//    List<Product> findAll();
 }
