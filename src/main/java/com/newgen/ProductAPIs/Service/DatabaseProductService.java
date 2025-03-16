48e7f837d09ce1646eba0434eee5ab205dd2a493
Prathamesh Gosavi <prathameshgosavi945@gmail.com>
2025-03-16 23:25:53 +0530

    add Service annotation on Product database service class

---
+++ src/main/java/com/newgen/ProductAPIs/Service/DatabaseProductService.java
@@ -0,0 +1,52 @@
+package com.newgen.ProductAPIs.Service;
+
+import com.newgen.ProductAPIs.model.Category;
+import com.newgen.ProductAPIs.model.Product;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class DatabaseProductService implements IProductService
+{
+
+    @Override
+    public void add(Product product) {
+        
+    }
+
+    @Override
+    public Product getProductById(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<Product> getAllProduct() {
+        return List.of();
+    }
+
+    @Override
+    public void deleteProduct(Long id) {
+
+    }
+
+    @Override
+    public List<Product> searchByCategory(Category category) {
+        return List.of();
+    }
+
+    @Override
+    public List<Product> searchByName(String name) {
+        return List.of();
+    }
+
+    @Override
+    public void updateProduct(Product newProduct) {
+
+    }
+
+    @Override
+    public List<Product> searchProdcutByRange(Double lowerPrice, Double higherPrice) {
+        return List.of();
+    }
+}
