ce9ce50521122c510c1babc348671017150a28dc
Prathamesh Gosavi <prathameshgosavi945@gmail.com>
2025-03-18 14:14:13 +0530

    change implementation

--- src/main/java/com/newgen/ProductAPIs/Service/DatabaseProductService.java
+++ src/main/java/com/newgen/ProductAPIs/Service/DatabaseProductService.java
@@ -14,7 +14,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
-@Primary
+//@Primary
 public class DatabaseProductService implements IProductService
 {
     private  ProductRepository productRepository;
--- src/main/java/com/newgen/ProductAPIs/Service/InMemoryProductService.java
+++ src/main/java/com/newgen/ProductAPIs/Service/InMemoryProductService.java
@@ -14,6 +14,7 @@ import java.util.*;
 
 
 @Service
+@Primary
 public class InMemoryProductService implements  IProductService {
 
     private final Map<Long, Product> products;
