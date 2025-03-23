c8d80411e84db85327b900ee6a8fef9f75025fa7
Prathamesh Gosavi <prathameshgosavi945@gmail.com>
2025-03-23 20:31:09 +0530

    add some oject mapping

--- src/main/java/com/newgen/ProductAPIs/controller/SampleController.java
+++ src/main/java/com/newgen/ProductAPIs/controller/SampleController.java
@@ -1,9 +1,13 @@
 package com.newgen.ProductAPIs.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.newgen.ProductAPIs.model.Post;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @RestController
 @Slf4j
@@ -16,15 +20,30 @@ public class SampleController {
     }
 
     @GetMapping("/posts")
-    public String getPost()
+    public List<Post> getPost()
     {
         RestTemplate restTemplate=new RestTemplate();
        String response= restTemplate.getForObject("https://jsonplaceholder.typicode.com/posts",String.class);
         log.info("get post API CALLED :{}" ,response);
-        return response;
+
         // search the  deleniti word
 
-        response.contains("deleniti");
+     //String -> List<Post>
+         ObjectMapper objectMapper=new ObjectMapper();
+
+        try
+        {
+            List<Post> posts= objectMapper.readValue(response,List.class);
+            log.info("Response Post {}:",posts);
+            return posts;
+
+        }
+        catch (Exception e)
+        {
+            log.error("Exception occur while parsing the response{}:", e);
+        }
+        List<Post> posts;
+        return null;
     }
 
 }
--- src/main/java/com/newgen/ProductAPIs/model/Post.java
+++ src/main/java/com/newgen/ProductAPIs/model/Post.java
@@ -1,6 +1,20 @@
 package com.newgen.ProductAPIs.model;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Data
 public class Post {
 
-    
+    private int userId;
+
+    private   int id;
+
+    private  String title;
+
+    private  String body;
+
 }
