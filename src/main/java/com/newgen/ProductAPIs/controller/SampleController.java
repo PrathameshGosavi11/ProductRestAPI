b6ed3e0e6191ad12b31566340ad47143581d6d51
Prathamesh Gosavi <prathameshgosavi945@gmail.com>
2025-03-23 13:36:35 +0530

    add restTemplate

--- src/main/java/com/newgen/ProductAPIs/controller/SampleController.java
+++ src/main/java/com/newgen/ProductAPIs/controller/SampleController.java
@@ -1,9 +1,12 @@
 package com.newgen.ProductAPIs.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
+@Slf4j
 public class SampleController {
 
     @GetMapping()
@@ -15,6 +18,11 @@ public class SampleController {
     @GetMapping("/post")
     public String getPost()
     {
+        RestTemplate restTemplate=new RestTemplate();
+        String response= restTemplate.getForObject("https://jsonplaceholder.typicode.com/posts",String.class);
+
+        //System.out.println("API response is :"+response);
+        log.info(response);
         return "Hello your POST";
     }
 }
